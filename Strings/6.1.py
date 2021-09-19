109130697faa318eabd34c11aaad80b28a45456a
peram-vignesh <90543677+peram-vignesh@users.noreply.github.com>
2021-09-19 15:55:04 +0530

    Add files via upload

---
+++ Strings/6.1.py
@@ -0,0 +1,18 @@
+string=input('Enter the string: ')
+count=0
+while count<len(string):
+    letter=string[count]
+    if letter == ":" :
+        break
+    count=count+1
+print (count)
+length=len(string)
+count=count+1
+cut=string[count:length]
+if cut.isnumeric()== True :
+    cut=float(cut)
+        
+else:
+    cut='you entered a string after colon'
+
+print (cut)
